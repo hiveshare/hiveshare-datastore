85b74e09ca6dd8e27fb4863b2de98621de9e8e5b
William Becker <wbecker+hiveshare@gmail.com>
2012-12-15 22:38:44 +0000

    create objects

--- server.js
+++ server.js
@@ -1,16 +1,62 @@
+var _ = require("underscore");
+var when = require("when");
+
 var mongodb = require("mongodb");
+
 module.exports = {
-  start: function (cb) {
+
+  start: function (collectionSuffix) {
+
     var mongoserver = new mongodb.Server("127.0.0.1", 27017, {});
     var db_connector = new mongodb.Db("hiveshare", mongoserver, {w: 1});
     
-    db_connector.open(function (err, db) {
-      this.connection = db;
-      cb(true);
-    });
+    var deferred = when.defer();
+
+    db_connector.open(_.bind(function (err, db) {
+
+      if (!err) {
+
+        this.connection = db;
+        this.objectConnection = new mongodb.Collection(this.connection, 
+          "object" + (collectionSuffix ? ("_" + collectionSuffix) : ""));
+        deferred.resolve(true);
+
+      } else {
+
+        deferred.reject(err);
+
+      }
+
+    }, this));
+    
+    return deferred.promise;
+
   },
+
+  createObject: function (opts) {
+    var deferred = when.defer();
+
+    this.objectConnection.insert({}, function (err, doc) {
+      if (!err) {
+        deferred.resolve(doc.length && doc[0]._id);
+      } else {
+        deferred.reject(err);
+      }
+    });
+
+    return deferred.promise;
+  },
+
+  addObjectType: function (id) {
+  },
+
   //TODO: make a proper query object
-  getObjects: function (query, cb) {
-    cb(null, {types: [{id: 1}]});
+  getObjects: function (query) {
+    var deferred = when.defer();
+
+    deferred.resolve({types: [{id: 1}]});
+
+    return deferred.promise;
   }
+
 };
--- test/server-test.js
+++ test/server-test.js
@@ -1,5 +1,6 @@
 var vows = require("vows");
 var assert = require("assert");
+var pipeline = require("when/pipeline");
 
 var suite = vows.describe("server");
 
@@ -9,19 +10,60 @@ suite.addBatch({
 
   "Objects": {
 
+    "Can be added": {
+
+      topic: function () {
+        var cb = this.callback;
+        pipeline([
+          function () {
+            return server.start("test"); 
+          },
+          function () {
+            return server.createObject({});
+          }
+        ]).then(function (result) {
+          cb(null, result);
+        });
+      },
+
+      "has values": function (err, id) {
+        assert(!!id);
+      }
+
+    }
+
+  }
+});
+
+suite.addBatch({
+
+  "Objects": {
+
     "Can be queried based on type": {
 
       topic: function () {
         var cb = this.callback;
-        server.start(function () {
-          server.getObjects({type: {id: 1}}, cb);
+        pipeline([
+          function () {
+            return server.start("test");
+          },
+          function () {
+            return server.createObject({});
+          },
+          function () {
+            return server.getObjects({type: {id: 1}});
+          }
+        ]).then(function (result) {
+          cb(null, result);
         });
       },
 
       "is correct": function (result) {
         assert.deepEqual(result, {types: [{id: 1}]});
       }
+
     }
+
   }
 });
 
